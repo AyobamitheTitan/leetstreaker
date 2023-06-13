c1774c6751569dac04c9022c02c90ae4426b38a3
Oluwatimilehin <adeoluoyewo13@gmail.com>
2023-06-13 19:48:45 +0100

    Fixing styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   purge: { content: ["./src/**/*.{html,ts}"], enable: true },
-//   theme: {
-//     extend: {},
-//   },
-//   variants: {},
-//   plugins: [require("tailwindcss"), require("autoprefixer")],
-// };
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
