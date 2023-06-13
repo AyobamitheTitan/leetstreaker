a538cc5d45cfa24e05a5e3c43d486c124af5bd17
Oluwatimilehin <adeoluoyewo13@gmail.com>
2023-06-13 20:18:57 +0100

    Purging my css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: ["./src/**/*.{html,ts}"],
+  },
+
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
